d2d2b5f05cfa3d8c7ef9c29b4fcebae4ac1b95c7
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2025-03-20 00:37:37 -0500

    Agregando rutas de home

--- e-commerce/microfrontends/projects/shell/src/app/app.config.ts
+++ e-commerce/microfrontends/projects/shell/src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
 
-import { routes } from './app.routes';
+
+
 import { provideClientHydration } from '@angular/platform-browser';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent } // Redirección inicial
+];
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration()]
