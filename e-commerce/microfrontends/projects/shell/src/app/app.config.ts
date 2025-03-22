5348a1231cc3197d7d632f1783565cf2d1504ef4
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2025-03-21 21:02:54 -0500

    Agregando Componente Navbar en Shell

--- e-commerce/microfrontends/projects/shell/src/app/app.config.ts
+++ e-commerce/microfrontends/projects/shell/src/app/app.config.ts
@@ -5,10 +5,12 @@ import { provideRouter, Routes } from '@angular/router';
 
 import { provideClientHydration } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent } // Redirección inicial
+  { path: 'home', component: HomeComponent },
+  { path: 'navbar', component:NavbarComponent}
 ];
 
 
