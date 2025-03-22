f9533d348481f5e363cdd7ae5e9880effdc565e9
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2025-03-22 17:06:29 -0500

    Arreglando Responsividad del navbar

--- e-commerce/microfrontends/projects/shell/src/app/app.component.ts
+++ e-commerce/microfrontends/projects/shell/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { NavbarInferiorComponent } from "./navbar-inferior/navbar-inferior.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, NavbarComponent],
+  imports: [RouterOutlet, RouterLink, NavbarComponent,NavbarInferiorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
