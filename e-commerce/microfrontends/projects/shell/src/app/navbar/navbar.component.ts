b693ac78454b8a13466b3ec07491b5f7f4b89c25
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2025-03-22 21:26:13 -0500

    Navbar completada, falta responsividad

--- e-commerce/microfrontends/projects/shell/src/app/navbar/navbar.component.ts
+++ e-commerce/microfrontends/projects/shell/src/app/navbar/navbar.component.ts
@@ -9,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+   // Contador de productos en el carrito
+   cartCount: number = 0;
+
+   // Función para agregar productos al carrito
+   addToCart() {
+     this.cartCount++;  // Aumenta el número de productos en el carrito
+     this.updateCartCount();  // Actualiza el contador en la vista
+   }
+ 
+   // Función para actualizar el contador
+   updateCartCount() {
+     if (this.cartCount > 0) {
+       // El contador será visible si hay productos en el carrito
+       // No se necesita hacer nada aquí si ya estamos usando la interpolación en el HTML
+     } else {
+       // Si no hay productos, se ocultan los elementos del carrito
+       this.cartCount = 0;
+     }
+   }
+
 }
